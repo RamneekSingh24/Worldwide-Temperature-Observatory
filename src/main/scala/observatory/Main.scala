67a0bab5e5506dcb0921a0dad2abc322ae9c4cc1
RamneekSingh24 <ramneeksingh2001@gmail.com>
2021-12-03 03:30:02 +0530

    visulization optimized using RTree

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -12,7 +12,8 @@ import scala.io.Source
 import observatory.Extraction
 import observatory.Extraction.getYearlyData
 import com.github.plokhotnyuk.rtree2d.core._
-import EuclideanPlane._
+import SphericalEarth._
+import observatory.Visualization.{RADIUS_EARTH_KM, degreeToRadian}
 
 object Main extends App {
 
@@ -46,6 +47,51 @@ object Main extends App {
 //  assert(rtree.searchAll(2.0f, 2.0f) == Seq(box1, box2))
 //  assert(rtree.searchAll(2.5f, 2.5f, 3.5f, 3.5f) == Seq(box2))
 //  assert(rtree.searchAll(1.5f, 1.5f, 2.5f, 2.5f).forall(entries.contains))
+
+
+
+  val city1 = entry(50.0614f, 19.9383f, "Kraków")
+  val city2 = entry(50.4500f, 30.5233f, "Kyiv")
+  //val entries = Seq(city1, city2)
+//
+//  val rtree = RTree(entries, nodeCapacity = 4/* the best capacity for nearest queries for spherical geometry */)
+//
+//  assert(rtree.entries == entries)
+//  assert(rtree.nearestOption(0.0f, 0.0f).contains(city1))
+//  assert(rtree.nearestOption(50f, 20f, maxDist = 1.0f).isEmpty)
+//  assert(rtree.nearestK(50f, 20f, k = 1) == Seq(city1))
+//  assert(rtree.nearestK(50f, 20f, k = 3, maxDist = 1000f) == Seq(city2, city1))
+//  assert(rtree.searchAll(50f, 30f, 51f, 31f) == Seq(city2))
+//  assert(rtree.searchAll(0f, -180f, 90f, 180f).forall(entries.contains))
+//
+  def distance(p1 : Location, p2 : Location) = {
+    val l1 = Math.sin(degreeToRadian(p1.lat)) * Math.sin(degreeToRadian(p2.lat))
+    val l2 = Math.cos(degreeToRadian(p1.lat)) * Math.cos(degreeToRadian(p2.lat))
+    val l3 = Math.cos(degreeToRadian(p1.lon) - degreeToRadian(p2.lon))
+    val centralAngle = Math.acos(l1 + l2 * l3)
+    RADIUS_EARTH_KM *  centralAngle
+
+  }
+//
+//  val l1 = Location(23f, 50f)
+//  val l2 = Location(40f, 69f);
+//  val entry1 = entry(l1.lat.toFloat, l1.lon.toFloat, "olabb")
+//  val entry2 = entry(l1.lat.toFloat, l1.lon.toFloat, "bbstudy")
+//  val entries = Seq(entry1, entry2)
+//  val rtree = RTree(entries)
+//
+//  val d1 = SphericalEarth.distanceCalculator.distance(l2.lat.toFloat, l2.lon.toFloat, rtree)
+//  val d2 = distance(l1, l2)
+//
+//  println(d1, d2)
+  //(2599.8354,2599.887880367632)
+
+
+
+
+
+
+
 
 
 }
--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,19 +1,70 @@
 package observatory
 
+import com.github.plokhotnyuk.rtree2d.core.RTree
 import com.sksamuel.scrimage.{Image, Pixel}
+import com.github.plokhotnyuk.rtree2d.core.SphericalEarth._
 
+import scala.math.Numeric.BigIntIsIntegral.mkNumericOps
 /**
   * 2nd milestone: basic visualization
   */
-object Visualization extends VisualizationInterface {
+object Visualization{
+
+
+  def degreeToRadian(theta : Double) = theta * Math.PI / 180.0
+
+  val RADIUS_EARTH_KM = 6371.137
+  val INTERPOLATION_CUTOFF = 1.0f
+  val P_VAL = 3
+  val EPS = 1e-7
+  val REQ_NEAREST = 1000;
+
+  val ALPHA = 255
+  val WIDTH = 360
+  val HEIGHT = 180
+
+
+
+  val colorLegend : Map[Temperature, Color] = Map[Temperature, Color](
+    +7.0 -> Color(0, 0, 0),   /* Black */
+    +4.0 -> Color(255, 0, 0),        /* Red */
+    +2.0 -> Color(255, 255, 0),      /* Yellow */
+    0.0 -> Color(255, 255, 255),    /* White */
+    -2.0 -> Color(0, 255, 255),      /* Light blue */
+    -7.0 -> Color(0, 0, 255)         /* Deep Blue */
+  )
+
+  //points.toList.sortWith(_._1 < _._1).toArray
+
+  /** @return Distance b/w points in KM */
+
+  def distance(p1 : Location, p2 : Location) = {
+    val l1 = Math.sin(degreeToRadian(p1.lat)) * Math.sin(degreeToRadian(p2.lat))
+    val l2 = Math.cos(degreeToRadian(p1.lat)) * Math.cos(degreeToRadian(p2.lat))
+    val l3 = Math.cos(degreeToRadian(p1.lon) - degreeToRadian(p2.lon))
+    val centralAngle = Math.acos(l1 + l2 * l3)
+    RADIUS_EARTH_KM *  centralAngle
+  }
+
+
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+
+
+  def predictTemperature(temperatures : RTree[(Location, Temperature)], location: Location): Temperature = {
+    val nearest = temperatures.nearestK(location.lat.toFloat, location.lon.toFloat, 1,  maxDist = INTERPOLATION_CUTOFF);
+    if (nearest.nonEmpty) nearest(0).value._2
+    else {
+      val nearest = temperatures.nearestK(location.lat.toFloat, location.lon.toFloat, REQ_NEAREST).map(_.value)
+      val weights = nearest.par.map(dt => (Math.pow(distance(dt._1, location), -P_VAL),dt._2))
+      val num = weights.par.foldLeft(0.0)((accum, curr) => accum + curr._1 * curr._2)
+      val denom = weights.par.foldLeft(0.0)((accum, curr) => accum + curr._1)
+      num / denom
+    }
   }
 
   /**
@@ -21,18 +72,54 @@ object Visualization extends VisualizationInterface {
     * @param value The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
-  def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+  def interpolateColor(sortedPoints: Array[(Double, Color)], value: Double): Color = {
+
+    for (i <- 0 until sortedPoints.length - 1) {
+      (sortedPoints(i), sortedPoints(i + 1)) match {
+        case ((v1, Color(r1, g1, b1)), (v2, Color(r2, g2, b2))) => {
+          if (v1 > value) {
+            return Color(r1, g1, b1)
+          }
+          else if (v2 > value) {
+            val ratio = (value - v1) / (v2 - v1)
+            return Color(
+              math.round(r1 + (r2 - r1) * ratio).toInt,
+              math.round(g1 + (g2 - g1) * ratio).toInt,
+              math.round(b1 + (b2 - b1) * ratio).toInt
+            )
+          }
+        }
+      }
+    }
+    sortedPoints(sortedPoints.length - 1)._2
   }
 
-  /**
-    * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @return A 360×180 image where each pixel shows the predicted temperature at its location
-    */
-  def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
-  }
+
+    def colorToPixel(c: Color): Pixel = {
+      Pixel.apply(c.red, c.green, c.blue, ALPHA)
+    }
+
+    def xyToLocation(x: Int, y: Int): Location = Location(90 - y, x - 180)
+
+
+    def visualize(temperatures: Iterable[(Location, Double)]): Image = {
+
+      val colors = colorLegend.toArray.sortWith(_._1 < _._1).toArray
+      val entries = temperatures.map(e => entry(e._1.lat.toFloat, e._1.lon.toFloat, e))
+      val temperaturesRTree = RTree(entries)
+
+      val buffer = new Array[Pixel](WIDTH * HEIGHT)
+      for (y <- 0 until HEIGHT) {
+        for (x <- 0 until WIDTH) {
+          val predictedTemp = predictTemperature(temperaturesRTree, xyToLocation(x, y))
+          buffer(y * WIDTH + x) = colorToPixel(Visualization.interpolateColor(colors, predictedTemp))
+        }
+      }
+      Image(WIDTH, HEIGHT, buffer)
+    }
 
 }
+
+
+
 
